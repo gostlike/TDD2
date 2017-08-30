5024b4c870184486465d21a1ba6ca82653cedb88
HarryYao <gostlike@gmail.com>
2017-08-30 21:08:07 +0800

    8.(綠燈)PotterShoppingCart價格計算測試_第一二三集買2本_第四五集各買1本_最優惠價格為640 7.(綠燈)PotterShoppingCart價格計算測試_第一集買一本_第二三集各買兩本_其中三本滿足9折兩本滿足95折優惠_價格為460 6.(綠燈)PotterShoppingCart價格計算測試_第一二集各買一本_第三集買兩本_其中三本滿足9折優惠_價格為370 5.(綠燈)PotterShoppingCart價格計算測試_第一二三四五集各買一本_滿足75折優惠_價格為375 4.(綠燈)PotterShoppingCart價格計算測試_第一二三四集各買一本_滿足8折優惠_價格為320 3.(綠燈)PotterShoppingCart價格計算測試_第一二三集各買一本_滿足9折優惠_價格為270 2.(綠燈)PotterShoppingCart價格計算測試_第一集買一本_第二集買一本_滿足95折優惠_價格為190 1.(綠燈)PotterShoppingCart價格計算測試_第一集買一本_無滿足任何優惠_價格為100

--- PotterShoppingCart.Tests/PotterShoppingCartPriceTest.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartPriceTest.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections.Generic;
-using ExpectedObjects;
+﻿using ExpectedObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ShoppingCartLibrary;
 using ShoppingCartLibrary.Models;
+using System.Collections.Generic;
 
 namespace PotterShoppingCart.Tests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod] 
-        public void PotterShoppingCart價格計算測試_第一集買一本_無滿足任何優惠_價格為100()
+        [TestMethod]
+        public void PotterShoppingCart價格計算測試_第一集買1本_無滿足任何優惠_價格為100()
         {
             var pottershoppingcart = new List<Book>
             {
@@ -25,8 +25,9 @@ namespace PotterShoppingCart.Tests
             var actual = pottershoppingcart.GetDiscountSum<Book>(discountrules, sumBy => sumBy.Price, groupby => groupby.Name);
             expected.ToExpectedObject().ShouldEqual(actual);
         }
+
         [TestMethod]
-        public void PotterShoppingCart價格計算測試_第一集買一本_第二集買一本_滿足95折優惠_價格為190()
+        public void PotterShoppingCart價格計算測試_第一集買1本_第二集買1本_滿足95折優惠_價格為190()
         {
             var pottershoppingcart = new List<Book>
             {
@@ -42,8 +43,9 @@ namespace PotterShoppingCart.Tests
             var actual = pottershoppingcart.GetDiscountSum<Book>(discountrules, sumBy => sumBy.Price, groupby => groupby.Name);
             expected.ToExpectedObject().ShouldEqual(actual);
         }
+
         [TestMethod]
-        public void PotterShoppingCart價格計算測試_第一二三集各買一本_滿足9折優惠_價格為270()
+        public void PotterShoppingCart價格計算測試_第一二三集各買1本_滿足9折優惠_價格為270()
         {
             var pottershoppingcart = new List<Book>
             {
@@ -60,8 +62,9 @@ namespace PotterShoppingCart.Tests
             var actual = pottershoppingcart.GetDiscountSum<Book>(discountrules, sumBy => sumBy.Price, groupby => groupby.Name);
             expected.ToExpectedObject().ShouldEqual(actual);
         }
+
         [TestMethod]
-        public void PotterShoppingCart價格計算測試_第一二三四集各買一本_滿足8折優惠_價格為320()
+        public void PotterShoppingCart價格計算測試_第一二三四集各買1本_滿足8折優惠_價格為320()
         {
             var pottershoppingcart = new List<Book>
             {
@@ -80,8 +83,9 @@ namespace PotterShoppingCart.Tests
             expected.ToExpectedObject().ShouldEqual(actual);
             //Assert.Fail("尚未實作");
         }
+
         [TestMethod]
-        public void PotterShoppingCart價格計算測試_第一二三四五集各買一本_滿足75折優惠_價格為375()
+        public void PotterShoppingCart價格計算測試_第一二三四五集各買1本_滿足75折優惠_價格為375()
         {
             var pottershoppingcart = new List<Book>
             {
@@ -100,8 +104,9 @@ namespace PotterShoppingCart.Tests
             var actual = pottershoppingcart.GetDiscountSum<Book>(discountrules, sumBy => sumBy.Price, groupby => groupby.Name);
             expected.ToExpectedObject().ShouldEqual(actual);
         }
+
         [TestMethod]
-        public void PotterShoppingCart價格計算測試_第一二集各買一本_第三集買兩本_其中三本滿足9折優惠_價格為370()
+        public void PotterShoppingCart價格計算測試_第一二集各買1本_第三集買2本_其中三本滿足9折優惠_價格為370()
         {
             var pottershoppingcart = new List<Book>
             {
@@ -119,8 +124,9 @@ namespace PotterShoppingCart.Tests
             var actual = pottershoppingcart.GetDiscountSum<Book>(discountrules, sumBy => sumBy.Price, groupby => groupby.Name);
             expected.ToExpectedObject().ShouldEqual(actual);
         }
+
         [TestMethod]
-        public void PotterShoppingCart價格計算測試_第一集買一本_第二三集各買兩本_其中三本滿足9折兩本滿足95折優惠_價格為460()
+        public void PotterShoppingCart價格計算測試_第一集買1本_第二三集各買2本_其中三本滿足9折兩本滿足95折優惠_價格為460()
         {
             var pottershoppingcart = new List<Book>
             {
@@ -141,5 +147,30 @@ namespace PotterShoppingCart.Tests
             expected.ToExpectedObject().ShouldEqual(actual);
         }
 
+        [TestMethod]
+        public void PotterShoppingCart價格計算測試_第一二三集買2本_第四五集各買1本_最優惠價格為640()
+        {
+            var pottershoppingcart = new List<Book>
+            {
+                new Book("Harry Potter Episode 1",100,1),
+                new Book("Harry Potter Episode 1",100,1),
+                new Book("Harry Potter Episode 2",100,1),
+                new Book("Harry Potter Episode 2",100,1),
+                new Book("Harry Potter Episode 3",100,1),
+                new Book("Harry Potter Episode 3",100,1),
+                new Book("Harry Potter Episode 4",100,1),
+                new Book("Harry Potter Episode 5",100,1)
+            };
+            var discountrules = new List<DiscountRule>
+            {
+                //第一個DiscountRule  > 第二個DiscountRule...依序優先套用
+                new DiscountRule() {Discount = 0.8, DifferentItemNumber = 4},
+                new DiscountRule() {Discount = 0.9, DifferentItemNumber = 3},
+                new DiscountRule() {Discount = 0.95, DifferentItemNumber = 2}
+            };
+            double expected = 640;
+            var actual = pottershoppingcart.GetDiscountSum<Book>(discountrules, sumBy => sumBy.Price, groupby => groupby.Name);
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
     }
 }
