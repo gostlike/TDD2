13e9e5a150b2f7e6240dcb62c8ee662529209c83
HarryYao <gostlike@gmail.com>
2017-08-30 20:12:50 +0800

    4.PotterShoppingCart價格計算測試_第一二三四集各買一本_滿足8折優惠_價格為320(綠燈) 3.PotterShoppingCart價格計算測試_第一二三集各買一本_滿足9折優惠_價格為270(綠燈) 2.PotterShoppingCart價格計算測試_第一集買一本_第二集買一本_滿足95折優惠_價格為190(綠燈) 1.PotterShoppingCart價格計算測試_第一集買一本_無滿足任何優惠_價格為100(綠燈)

--- PotterShoppingCart.Tests/PotterShoppingCartPriceTest.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartPriceTest.cs
@@ -54,7 +54,25 @@ namespace PotterShoppingCart.Tests
             double expected = 270;
             var actual = pottershoppingcart.GetDiscountSum<Book>(discountrule, sumBy => sumBy.Price, groupby => groupby.Name);
             expected.ToExpectedObject().ShouldEqual(actual);
+        }
+        [TestMethod]
+        public void PotterShoppingCart價格計算測試_第一二三四集各買一本_滿足8折優惠_價格為320()
+        {
+            var pottershoppingcart = new List<Book>
+            {
+                new Book("Harry Potter Episode 1",100,1),
+                new Book("Harry Potter Episode 2",100,1),
+                new Book("Harry Potter Episode 3",100,1),
+                new Book("Harry Potter Episode 4",100,1)
+            };
+            var discountrule =
+                new DiscountRule() { Discount = 0.8, DifferentItemNumber = 4 };
+
+            double expected = 320;
+            var actual = pottershoppingcart.GetDiscountSum<Book>(discountrule, sumBy => sumBy.Price, groupby => groupby.Name);
+            expected.ToExpectedObject().ShouldEqual(actual);
             //Assert.Fail("尚未實作");
         }
+
     }
 }
